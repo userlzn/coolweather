6c868ba985fafd70f78c5acaeda5a72ebfa83570
lvzhognnan <1060996687@qq.com>
2017-03-10 15:42:28 +0800

    First commit.

---
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);//c980e7c005a240c8ac431ed5de4adf92
+                                                 //https://github.com/userlzn/coolweather.git
+    }
+}
